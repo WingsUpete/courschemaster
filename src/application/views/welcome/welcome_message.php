7d10d860f08855f3aec7fbf160cc63a619744f19
SuntCrick <43040147+SuntCrick@users.noreply.github.com>
2019-11-21 02:02:07 +0800

    change the icons of welcome page

--- src/application/views/welcome/welcome_message.php
+++ src/application/views/welcome/welcome_message.php
@@ -6,7 +6,7 @@
 				<div class="col-sm-4">
 					<a href="<?= site_url('students') ?>">
 						<div class="nav-icons">
-							<i class="fas fa-chalkboard-teacher fa-4x"></i>
+							<i class="fas fa-user-edit fa-4x"></i>
 						</div>
 						<div class="nav-texts">
 							<h3>Student</h3>
@@ -16,7 +16,7 @@
 				<div class="col-sm-4">
 					<a href="<?= site_url('staff') ?>">
 						<div class="nav-icons">
-							<i class="fas fa-user-cog fa-4x"></i>
+							<i class="fas fa-user-tie fa-4x"></i>
 						</div>
 						<div class="nav-texts">
 							<h3>Staff</h3>
@@ -26,7 +26,7 @@
 				<div class="col-sm-4">
 					<a href="<?= site_url('visitors') ?>">
 						<div class="nav-icons">
-							<i class="fas fa-user-edit fa-4x"></i>
+							<i class="fas fa-user-friends fa-4x"></i>
 						</div>
 						<div class="nav-texts">
 							<h3>Visitor</h3>
