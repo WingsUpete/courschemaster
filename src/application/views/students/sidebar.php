c8aa02c7a1747e29f9d9fec78d2b5f7246e7ce3a
Peter S <petershen815@126.com>
2019-11-29 02:56:25 +0800

    really nb sidebar.php

--- src/application/views/students/sidebar.php
+++ src/application/views/students/sidebar.php
@@ -2,17 +2,37 @@
 		<a id="closeNav" href="javascript:void(0);" class="closebtn">&times;</a>
 		<div class="sidenav-items">
 			<?php $active = ($active_sidebar == PRIV_STUDENTS_MY_COURSCHEMA) ? 'active' : '' ?>
-			<a href="<?= site_url('students') ?>" class="<?= $active ?>">My Courschema</a>
+			<a href="<?= site_url('students') ?>" class="<?= $active ?>">
+				<span class="sd_icn"><i class="fas fa-book"></i></span>
+				&ensp;
+				<span class="sd_epn"><?= lang('my_courschema') ?></span>
+			</a>
 			<?php $active = ($active_sidebar == PRIV_STUDENTS_ALL_COURSCHEMAS) ? 'active' : '' ?>
-			<a href="<?= site_url('students/all_courschemas') ?>" class="<?= $active ?>">All Courschemas</a>
+			<a href="<?= site_url('students/all_courschemas') ?>" class="<?= $active ?>">
+				<span class="sd_icn"><i class="fas fa-layer-group"></i></span>
+				&ensp;
+				<span class="sd_epn"><?= lang('all_courschemas') ?></span>
+			</a>
 			<?php $active = ($active_sidebar == PRIV_STUDENTS_COLLECTION) ? 'active' : '' ?>
-			<a href="<?= site_url('students/collection') ?>" class="<?= $active ?>">Collection</a>
+			<a href="<?= site_url('students/collection') ?>" class="<?= $active ?>">
+				<span class="sd_icn"><i class="fas fa-star"></i></span>
+				&ensp;
+				<span class="sd_epn"><?= lang('collection') ?></span>
+			</a>
 			<hr />
 			<?php $active = ($active_sidebar == PRIV_STUDENTS_MY_PLAN) ? 'active' : '' ?>
-			<a href="<?= site_url('students/my_plan') ?>" class="<?= $active ?>">My Plan</a>
+			<a href="<?= site_url('students/my_plan') ?>" class="<?= $active ?>">
+				<span class="sd_icn"><i class="fas fa-trophy"></i></span>
+				&ensp;
+				<span class="sd_epn"><?= lang('my_plan') ?></span>
+			</a>
 			<hr />
 			<?php $active = ($active_sidebar == PRIV_STUDENTS_LEARNED) ? 'active' : '' ?>
-			<a href="<?= site_url('students/learned') ?>" class="<?= $active ?>">Learned</a>
+			<a href="<?= site_url('students/learned') ?>" class="<?= $active ?>">
+				<span class="sd_icn"><i class="fas fa-graduation-cap"></i></span>
+				&ensp;
+				<span class="sd_epn"><?= lang('learned') ?></span>
+			</a>
 		</div>
 	</div>
 	
