28d4a0c4b8fad8a7a9fbf4686d84bb3fa5b7b75b
Peter S <petershen815@126.com>
2019-11-28 23:25:48 +0800

    update several styles of main page

--- src/application/views/general/footer.php
+++ src/application/views/general/footer.php
@@ -2,7 +2,7 @@
 	</div>
 	
 	<!-- Footer -->
-	<footer id="page-footer" class="page-footer blue">
+	<footer id="page-footer" class="page-footer">
 		<div class="container-fluid">
 			<div class="row">
 				<div class="col-xs-12 col-lg-5">
--- src/application/views/welcome/welcome_message.php
+++ src/application/views/welcome/welcome_message.php
@@ -5,41 +5,81 @@
     };
 </script>
 
-	<!-- Main content -->
-	<div id="welcome_page" class="main-content">
-		<div id="nav-components" class="text-black text-center">
-			<h1 class="nav-title">Courschemaster</h1>
-			<div class="row">
-				<div class="col-sm-4">
-					<a href="<?= site_url('students') ?>">
-						<div class="nav-icons">
-							<i class="fas fa-user-edit fa-4x"></i>
-						</div>
-						<div class="nav-texts">
-							<h3>Student</h3>
-						</div>
-					</a>
-				</div>
-				<div class="col-sm-4">
-					<a href="<?= site_url('staff') ?>">
-						<div class="nav-icons">
-							<i class="fas fa-user-tie fa-4x"></i>
-						</div>
-						<div class="nav-texts">
-							<h3>Staff</h3>
-						</div>
-					</a>
-				</div>
-				<div class="col-sm-4">
-					<a href="<?= site_url('visitors') ?>">
-						<div class="nav-icons">
-							<i class="fas fa-user-friends fa-4x"></i>
-						</div>
-						<div class="nav-texts">
-							<h3>Visitor</h3>
-						</div>
-					</a>
-				</div>
+<!-- Main content -->
+<div id="bg-img"></div>
+
+<div id="welcome_page" class="main-content">
+	<div id="nav-components--general" class="nav-components text-black text-center">
+<!--		<h1 class="nav-title">Courschemaster</h1>-->
+		<div class="row">
+			<div class="col-sm-4">
+				<a href="<?= site_url('students') ?>">
+					<div class="nav-icons">
+						<i class="fas fa-user-edit fa-4x"></i>
+					</div>
+					<div class="nav-texts">
+						<h3>Student</h3>
+					</div>
+				</a>
+			</div>
+			<div class="col-sm-4">
+				<a href="<?= site_url('staff') ?>">
+					<div class="nav-icons">
+						<i class="fas fa-user-tie fa-4x"></i>
+					</div>
+					<div class="nav-texts">
+						<h3>Staff</h3>
+					</div>
+				</a>
+			</div>
+			<div class="col-sm-4">
+				<a href="<?= site_url('visitors') ?>">
+					<div class="nav-icons">
+						<i class="fas fa-user-friends fa-4x"></i>
+					</div>
+					<div class="nav-texts">
+						<h3>Visitor</h3>
+					</div>
+				</a>
 			</div>
 		</div>
 	</div>
+	<div id="nav-components--staff" class="nav-components text-black text-center">
+<!--		<h1 class="nav-title"><i class="fas fa-user-tie fa-lg"></i>&ensp;Staff</h1>-->
+		<div class="row">
+			<div class="col-sm-4">
+				<a href="<?= site_url('staff') ?>">
+					<div class="nav-icons">
+						<i class="fas fa-user-cog fa-4x"></i>
+					</div>
+					<div class="nav-texts">
+						<h3>TAO</h3>
+					</div>
+				</a>
+			</div>
+			<div class="col-sm-4">
+				<a href="<?= site_url('staff') ?>">
+					<div class="nav-icons">
+						<i class="fas fa-user-secret fa-4x"></i>
+					</div>
+					<div class="nav-texts">
+						<h3>Secretary</h3>
+					</div>
+				</a>
+			</div>
+			<div class="col-sm-4">
+				<a href="<?= site_url('staff') ?>">
+					<div class="nav-icons">
+						<i class="fas fa-chalkboard-teacher fa-4x"></i>
+					</div>
+					<div class="nav-texts">
+						<h3>Mentor</h3>
+					</div>
+				</a>
+			</div>
+		</div>
+		<div id="staff-compo-back-block">
+			<i id="staff-compo-back" class="fas fa-chevron-circle-left fa-2x"></i>
+		</div>
+	</div>
+</div>
