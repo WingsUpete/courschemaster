4547cea52054d89ab83ffb9575229bc1b2b2be5a
mikechesterwang <52522981+mikechesterwang@users.noreply.github.com>
2019-11-28 23:36:55 +0800

    add language elements for the welcome page

--- src/application/language/english/translations_lang.php
+++ src/application/language/english/translations_lang.php
@@ -1,3 +1,9 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 $lang['confirm'] = 'Confirm';
 $lang['my_courschema'] = 'My Courschema';
+$lang['student'] = 'Student';
+$lang['staff'] = 'Staff';
+$lang['visitor'] = 'Visitor';
+$lang['tao'] = 'TAO';
+$lang['secretary'] = 'Secretary';
+$lang['mentor'] = 'Mentor';
--- src/application/language/简体中文/translations_lang.php
+++ src/application/language/简体中文/translations_lang.php
@@ -1,3 +1,9 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 $lang['confirm'] = '简体中文';
 $lang['my_courschema'] = '我的培养方案';
+$lang['student'] = '学生';
+$lang['staff'] = '教师';
+$lang['visitor'] = '访客';
+$lang['tao'] = '教务处';
+$lang['secretary'] = '教学秘书';
+$lang['mentor'] = '导师';
