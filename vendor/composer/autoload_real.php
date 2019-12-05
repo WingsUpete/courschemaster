5a244adde50f16f7f7bd25e41ca8c93a8f7fbfe6
Hui-Ouyang16 <11710106@mail.sustech.edu.cn>
2019-12-05 22:18:42 +0800

    add the packages about phpspeadsheet

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -47,6 +47,24 @@ class ComposerAutoloaderInit339dbf906695fc2f0e1b0526e0b94e71
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit339dbf906695fc2f0e1b0526e0b94e71::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire339dbf906695fc2f0e1b0526e0b94e71($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire339dbf906695fc2f0e1b0526e0b94e71($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
