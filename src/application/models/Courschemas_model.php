7c710fbf7be51dad81afc22c9229ee2a51cb70bf
mikechesterwang <52522981+mikechesterwang@users.noreply.github.com>
2019-12-08 21:33:14 +0800

    fixed today's ultimate bug

--- src/application/models/Courschemas_model.php
+++ src/application/models/Courschemas_model.php
@@ -1,6 +1,6 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Courschemas_model extends CI_Controller{
+class Courschemas_model extends CI_Model{
 
     public function get_dep($language){
         if($language == 'english'){
