4d583adb85f0b67fbc16c432ef403fc458ccc7fc
mikechesterwang <52522981+mikechesterwang@users.noreply.github.com>
2019-12-09 20:49:22 +0800

    update some bravo visual effect

--- src/application/views/general/update.php
+++ src/application/views/general/update.php
@@ -2,10 +2,21 @@
 <html>
 <head>
 	<title>Migration</title>
+
 </head>
+<script type="text/javascript">
+    var color_ptr = 0;
+    setInterval('changeColor()',20);
+        function changeColor(){
+            var component = document.getElementById('id1'); 
+            var colorArr=['#00FF00','#33FF33','#CCFFCC','#0066FF','#333366', '#6633FF', '#FF0000', '#FFFF00', '#9999FF', '#FF3300']; 
+			color_ptr = (color_ptr + 1)% colorArr.length;
+			component.style.backgroundColor = colorArr[color_ptr]; 
+		}
+</script>
 <body>
 
-<h1><?php echo $success ? 'Migration Success<br />Wow, you can really update.&ensp;<a href="' . site_url('') . '" style="text-decoration:none;color:#fff;background-color: purple;padding:20px;">back to home page</a>' : 'Failed to migrate: WTF?'; ?></h1>
+<h1 id="id1"><?php echo $success ? 'Migration Success<br />Wow, you can really update.&ensp;<a href="' . site_url('') . '" style="text-decoration:none;color:#fff;background-color: purple;padding:20px;">back to home page</a>' : 'Failed to migrate: WTF?'; ?></h1>
 
 <h1><?php echo $success ? '' : $exception; ?></h1>
 
