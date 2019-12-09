de7a7eb50f53462a32b4b739b7e0c5f7da8d216d
mikechesterwang <52522981+mikechesterwang@users.noreply.github.com>
2019-12-09 21:38:39 +0800

    migration view

--- src/application/views/general/update.php
+++ src/application/views/general/update.php
@@ -5,16 +5,18 @@
 
 </head>
 <script type="text/javascript">
-    var color_ptr = 0;
-    setInterval('changeColor()',20);
-        function changeColor(){
-            var component = document.getElementById('id1'); 
-            var colorArr=['#00FF00','#33FF33','#CCFFCC','#0066FF','#333366', '#6633FF', '#FF0000', '#FFFF00', '#9999FF', '#FF3300']; 
-			color_ptr = (color_ptr + 1)% colorArr.length;
-			component.style.backgroundColor = colorArr[color_ptr]; 
-		}
+	
+	var color_ptr = 0;
+	setInterval('changeColor()',20);
+	function changeColor(){
+		var component = document.getElementById('id1'); 
+		var colorArr=['#00FF00','#33FF33','#CCFFCC','#0066FF','#333366', '#6633FF', '#FF0000', '#FFFF00', '#9999FF', '#FF3300']; 
+		color_ptr = (color_ptr + 1)% colorArr.length;
+		component.style.backgroundColor = colorArr[color_ptr]; 
+	}
 </script>
 <body>
+<audio id="id2" src="<?= asset_url('assets/bgm/Freaks.mp3') ?>" controls="controls" autoplay loop="true" hidden="true"></audio>
 
 <h1 id="id1"><?php echo $success ? 'Migration Success<br />Wow, you can really update.&ensp;<a href="' . site_url('') . '" style="text-decoration:none;color:#fff;background-color: purple;padding:20px;">back to home page</a>' : 'Failed to migrate: WTF?'; ?></h1>
 
