3186888f0cbbfbe189cc8f84a0402de8677e37d5
mikechesterwang <52522981+mikechesterwang@users.noreply.github.com>
2019-12-11 11:13:04 +0800

    rename current_courschema_api

--- src/application/controllers/Current_courschema_api.php
+++ src/application/controllers/Current_courschema_api.php
@@ -1,6 +1,6 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-class All_courschemas_api extends CI_Controller{
+class Current_courschema_api extends CI_Controller{
     public function __construct(){
         parent::__construct();
 
