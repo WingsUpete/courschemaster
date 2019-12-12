58718699cd318955089b0405c493173e6e9f93f5
Vinson <1030898339@qq.com>
2019-12-12 15:00:47 +0800

    Matryona IDE

--- src/assets/js/cm_compiler_helper.js
+++ src/assets/js/cm_compiler_helper.js
@@ -1,82 +1,175 @@
-(function () {
+window.MatryonaTranslateClass = window.MatryonaTranslateClass || {};	// Browser Compatibility
 
-    'use strict';
+/**
+ *
+ * An example of a Static Class
+ *
+ * @module StaticClass
+ */
+(function (exports) {
 
-    /**
-     * StudentsAllCourschemasHelper Class
-     *
-     * This class contains the methods that are used in the Students My Courschema page.
-     *
-     * @class StudentsAllCourschemasHelper
-     */
-    function StudentsAllCourschemasHelper() {
-//        this.filterResults = {};
-    }
+	'use strict';	// strict mode execution: This means no undeclared variable usage.
 
-    /**
-     * Binds the default event handlers of the Students My Appointment page.
-     */
-    StudentsAllCourschemasHelper.prototype.bindEventHandlers = function () {
-        var instance = this;
+	// an example static method
+	// to call this method, write `StaticClass.exampleStaticMethod('Hi')`
+	exports.exampleStaticMethod = function (param0) {
+		// do sth
+		console.log(param0);
+	};
+	load = function (Matyrona_file_name){
+		let r = new XMLHttpRequest(),
+			okStatus = document.location.protocol === "file:" ? 0 : 200;
+		r.open('GET', name, false);
+		r.overrideMimeType("text/html;charset=utf-8");
+		r.send(null);
+		return r.status === okStatus ? r.responseText : null;
+	};
+	/**
+	 * @return {string}
+	 */
+	exports.Matryona_to_List = function (){
+		let courschema = load("courschema.cmc");
 
-		// Listners
-		
+		let english_req = load("english_req.cmh");
+
+		let political_class_req = load("political_class_req.cmh");
+
+		let public_elective_class_req = load("public_elective_class_req.cmh");
+
+		var all = courschema + english_req + political_class_req + public_elective_class_req;
+
+		//base information of courschema
+		var NAME = courschema.split("NAME = ")[1].split("\"")[1];
+		var EN_NAME = courschema.split("EN_NAME = ")[1].split("\"")[1];
+		var G = courschema.split("GROUP = (")[1].split(");")[0].replace(/\"/g, " ").split(") || (");
+		var GROUP = [];
+		for (var i=0; i<G.length; i++){
+			GROUP.push(G[i].replace(" && "," "));
+		}
+
+		var VERSION = courschema.split("VERSION = ")[1].split(";")[0];
+		var PROGRAM_LENGTH = courschema.split("PROGRAM_LENGTH = ")[1].split(";")[0];
+		var INTRO = courschema.split("INTRO = ")[1].split("\"")[1];
+		var EN_INTRO = courschema.split("EN_INTRO = ")[1].split("\"")[1];
+		var OBJECTIVES = courschema.split("OBJECTIVES = ")[1].split("\"")[1];
+		var EN_OBJECTIVES = courschema.split("EN_OBJECTIVES = ")[1].split("\"")[1];
+		var DEGREE = courschema.split("DEGREE = ")[1].split("\"")[1];
+		var EN_DEGREE = courschema.split("EN_DEGREE = ")[1].split("\"")[1];
+
+		//resolve event of courschema
+		var temp = all.split("Event ");
+
+		var list = [];
+		var nest_event;
+		var char;
+		var char_list = [];
+
+		var expression;
+
+		var event_list = [];
+		var event;
+		var event_name;
+
+		var description = {
+			name: NAME,
+			en_name: EN_NAME,
+			group: GROUP,
+			version: VERSION,
+			program_length: PROGRAM_LENGTH,
+			intro: INTRO,
+			en_intro: EN_INTRO,
+			objectives: OBJECTIVES,
+			en_objectives: EN_OBJECTIVES,
+			degree: DEGREE,
+			en_degree: EN_DEGREE
+		};
+		list.push(description);
+
+		//event object
+		for (var i = 1; i < temp.length; i++) {
+			if (temp[i].indexOf("GRADUATION") !== -1) {
+				event = {id: i - 1, name: "Graduation", rank:0};
+				event_list.push(event);
+			} else {
+				if (temp[i].indexOf("ComEvent") !== -1) {
+					event_name = temp[i].split("=")[0].replace(" ","");
+					event = {id: i - 1, name:event_name, rank:1};
+					event_list.push(event);
+				}else if (temp[i].indexOf("CourseEvent") !== -1){
+					event_name = temp[i].split("=")[0].replace(" ","");
+					event = {id: i - 1, name:event_name, rank:2};
+					event_list.push(event);
+				}else if (temp[i].indexOf("ScoreEvent") !== -1){
+					event_name = temp[i].split("=")[0].replace(" ","");
+					event = {id: i - 1, name:event_name, rank:3};
+					event_list.push(event);
+				}else if (temp[i].indexOf("VariableEvent") !== -1){
+					event_name = temp[i].split("=")[0].replace(" ","");
+					event = {id: i - 1, name:event_name, rank:4};
+					event_list.push(event)
+				}
+			}
+		}
+
+		//json
+		for (var j = 1; j < temp.length; j++) {
+
+			char_list = [];
+			if (event_list[j-1].rank === 4){
+				expression = temp[j].split("Event")[1].split(";")[0].split(", ");
+				char = ["variable", expression[0].replace("( ","")];
+				char_list.push(char);
+				event_name = expression[1].replace(" )","");
+				for (var l=0; l<event_list.length; l++){
+					if (event_name === event_list[l].name){
+						char = ["subevent", event_list[l].name, event_list[l].id];
+						break;
+					}
+				}
+				char_list.push(char);
+			}else if (event_list[j-1].rank === 3){
+				char = ["scorevent", temp[j].split("Event")[1].split(";")[0]];
+				char_list.push(char);
+			}else if (event_list[j-1].rank === 2){
+				expression = temp[j].split("Event")[1].split(";")[0].split(" ");
+				for (var k=0; k<expression.length; k++){
+					if (expression[k].indexOf("=") !== -1 || expression[k].indexOf("&&") !== -1 || expression[k].indexOf("^") !== -1 || expression[k].indexOf("||") !== -1 || expression[k].indexOf("!") !== -1 || expression[k].indexOf("(") !== -1 || expression[k].indexOf(")") !== -1){
+						char = ["operator", expression[k]];
+						char_list.push(char);
+					}else{
+						char = ["course", expression[k]];
+						char_list.push(char);
+					}
+				}
+			}else{
+				expression = temp[j].split("Event")[1].split(";")[0].split(" ");
+				for (var k=0; k<expression.length; k++){
+					if (expression[k].indexOf("=") !== -1 || expression[k].indexOf("&&") !== -1 || expression[k].indexOf("^") !== -1 || expression[k].indexOf("||") !== -1 || expression[k].indexOf("!") !== -1 || expression[k].indexOf("(") !== -1 || expression[k].indexOf(")") !== -1){
+						char = ["operator", expression[k]];
+						char_list.push(char);
+					}else{
+						for (var l=0; l<event_list.length; l++){
+							if (expression[k] === event_list[l].name){
+								char = ["subevent", event_list[l].name, event_list[l].id];
+								break;
+							}
+						}
+						char_list.push(char);
+					}
+				}
+			}
+
+			nest_event = {
+				event_id:event_list[j-1].id,
+				event_name:event_list[j-1].name,
+				event_rank:event_list[j-1].rank,
+				event_expression:char_list
+			};
+			list.push(nest_event);
+		}
+		return JSON.stringify(list);
 	};
 
-	//	Additional Methods
+})(window.MatryonaTranslateClass);
 
-    /**
-     * Get All Departments
-     */
-    StudentsAllCourschemasHelper.prototype.saveEdition = function () {
-        var tutor_id = $('#tutor-id').val();
-		var first_name = GeneralFunctions.superEscapeHTML($('#first-name').val());
-		var last_name = GeneralFunctions.superEscapeHTML($('#last-name').val());
-		var phone_number = GeneralFunctions.superEscapeHTML($('#phone-number').val());
-		var address = GeneralFunctions.superEscapeHTML($('#address').val());
-		var email = GeneralFunctions.superEscapeHTML($('#email').val());
-		var personal_page = $('#personal-page').val();
-		var introduction = GeneralFunctions.superEscapeHTML($('#introduction').val());
-		var flexible_column = GeneralFunctions.superEscapeHTML($('#flexible-column').val());
-		
-		//	AJAX
-        var postUrl = GlobalVariables.baseUrl + '/index.php/admin_api/ajax_edit_tutor';
-        var postData = {
-            csrfToken: GlobalVariables.csrfToken,
-			tutor_id : tutor_id,
-			first_name : JSON.stringify(first_name),
-			last_name : JSON.stringify(last_name),
-			personal_page : JSON.stringify(personal_page),
-			introduction : JSON.stringify(introduction),
-			address : JSON.stringify(address),
-			flexible_column : JSON.stringify(flexible_column),
-			email : JSON.stringify(email),
-			phone_number : JSON.stringify(phone_number)
-        };
-		
-		var obj = this;
 
-        $.post(postUrl, postData, function (response) {
-			//	Test whether response is an exception or a warning
-            if (!GeneralFunctions.handleAjaxExceptions(response)) {
-                return;
-            }
-			
-			if (response === 'success') {
-				Admin.displayNotification(EALang.edit_tutor_success, undefined, "success");
-			} else if (response === 'fail') {
-				Admin.displayNotification(EALang.edit_tutor_fail, undefined, "failure");
-			}
-			
-			var newName = first_name + " " + last_name;
-			$('.admin-page #tutor_config .results .entry.selected')[0].title = newName;
-			$('.admin-page #tutor_config .results .entry.selected strong.nameTags')[0].innerHTML = newName;
-			
-			//	Save, calendar needs refetching
-			this.calendar_needs_retrieval = true;
-			
-        }.bind(this), 'json').fail(GeneralFunctions.ajaxFailureHandler);
-    };
-	
-    window.StudentsAllCourschemasHelper = StudentsAllCourschemasHelper;
-})();
