254565dc2144744784273910b073a581a99ffa5d
Peter S <petershen815@126.com>
2019-12-18 13:58:05 +0800

    课程管理页面汉化，modal setup

--- src/application/views/general/cmManagement/course_management.php
+++ src/application/views/general/cmManagement/course_management.php
@@ -5,6 +5,7 @@
     var GlobalVariables = {
         csrfToken          : <?= json_encode($this->security->get_csrf_hash()) ?>,
 		baseUrl            : <?= json_encode($base_url) ?>
+		//	course_info, find it elsewhere
     };
 
     $(document).ready(function() {
@@ -18,16 +19,16 @@
 	<hr class="hide-for-now" />
 	<!-- Button group -->
 	<div class="row pl-3 pr-3 mb-4">
-		<button type="button" class="btn btn-sm add"><i class="fas fa-plus fa-lg"></i></button>
+		<button type="button" class="btn btn-sm add" data-toggle="tooltip" data-title="<?= lang('add') ?>"><i class="fas fa-plus fa-lg"></i></button>
 		&ensp;&ensp;
 		<div>
 		<div class="input-group">
 			<div class="custom-file">
 				<input type="file" class="custom-file-input" id="choose-file" aria-describedby="upload-file" />
-				<label class="custom-file-label" for="choose-file">Choose File</label>
+				<label class="custom-file-label" for="choose-file"><?= lang('choose_file') ?></label>
 			</div>
 			<div class="input-group-append">
-				<span class="input-group-text" id="upload-file" data-toggle="tooltip" data-title="Upload" data-placement="right"><i class="fas fa-upload fa-sm"></i></span>
+				<span class="input-group-text" id="upload-file" data-toggle="tooltip" data-title="<?= lang('upload') ?>" data-placement="right"><i class="fas fa-upload fa-sm"></i></span>
 			</div>
 		</div>
 		</div>
@@ -38,7 +39,7 @@
 			'Professional elective', 'General elective', 'Practice'
 		);
 		$course_table_headers = array(
-			'Course ID', 'Course Name', 'Total_Credit', 'Weekly Period', 'Department'
+			lang('course_code'), lang('course_name'), lang('course_total_credit'), lang('course_weekly_period'), lang('course_department')
 //			'Semester', 'Language', 'Experimental Credit', 'Advanced Placement', 'Course Description'
 		);
 	?>
@@ -68,61 +69,39 @@
 					</button>
 				</div>
 				<div class="modal-body">
-					<div class="md-form md-outline">
-						<input type="text" id="course_code" class="form-control is-invalid" maxlength="50" />
-						<label for="course_code" data-error="wrong" data-success="right">Code</label>
-						<div class="invalid-feedback"></div>
-					</div>
-					<div class="md-form md-outline">
-						<input type="text" id="course_chinese_name" class="form-control is-invalid" maxlength="150" />
-						<label for="course_chinese_name" data-error="wrong" data-success="right">Chinese Name</label>
-						<div class="invalid-feedback"></div>
-					</div>
-					<div class="md-form md-outline">
-						<input type="text" id="course_english_name" class="form-control is-invalid" maxlength="150" />
-						<label for="course_english_name" data-error="wrong" data-success="right">English Name</label>
-						<div class="invalid-feedback"></div>
-					</div>
-					<div class="md-form md-outline">
-						<input type="number" id="course_total_credit" class="form-control is-invalid" maxlength="150" />
-						<label for="course_total_credit" data-error="wrong" data-success="right">Total Credit</label>
-						<div class="invalid-feedback"></div>
-					</div>
-					<div class="md-form md-outline">
-						<input type="number" id="course_experiment_credit" class="form-control is-invalid" maxlength="150" />
-						<label for="course_experiment_credit" data-error="wrong" data-success="right">Experiment Credit</label>
-						<div class="invalid-feedback"></div>
-					</div>
-					<div class="md-form md-outline">
-						<input type="number" id="course_weekly_period" class="form-control is-invalid" maxlength="150" />
-						<label for="course_weekly_period" data-error="wrong" data-success="right">Weekly Period</label>
-						<div class="invalid-feedback"></div>
-					</div>
-					<div class="md-form md-outline">
-						<input type="text" id="course_semester" class="form-control is-invalid" maxlength="150" />
-						<label for="course_semester" data-error="wrong" data-success="right">Semester</label>
-						<div class="invalid-feedback"></div>
-					</div>
-					<div class="md-form md-outline">
-						<input type="text" id="course_language" class="form-control is-invalid" maxlength="150" />
-						<label for="course_language" data-error="wrong" data-success="right">Language</label>
-						<div class="invalid-feedback"></div>
-					</div>
-					<div class="md-form md-outline">
-						<textarea id="course_prelogic" class="md-textarea form-control is-invalid" rows="5" maxlength="250" style="resize:none;"></textarea>
-						<label for="course_prelogic" data-error="wrong" data-success="right">Pre-logic</label>
-						<div class="invalid-feedback"></div>
-					</div>
-					<div class="md-form md-outline">
-						<textarea id="course_description" class="md-textarea form-control is-invalid" rows="5" maxlength="250" style="resize:none;"></textarea>
-						<label for="course_description" data-error="wrong" data-success="right">Description</label>
-						<div class="invalid-feedback"></div>
-					</div>
-					<div class="md-form md-outline">
-						<textarea id="course_english_description" class="md-textarea form-control is-invalid" rows="5" maxlength="250" style="resize:none;"></textarea>
-						<label for="course_english_description" data-error="wrong" data-success="right">English Description</label>
-						<div class="invalid-feedback"></div>
-					</div>
+					<?php
+						// id, input / textarea, input type / textarea rows, display
+						$course_info = array(
+							array('course_code', 'input', 'text', lang('course_code')),
+							array('course_chinese_name', 'input', 'text', lang('course_chinese_name')),
+							array('course_english_name', 'input', 'text', lang('course_english_name')),
+							array('course_department', 'input', 'text', lang('course_department')),
+							array('course_total_credit', 'input', 'number', lang('course_total_credit')),
+							array('course_experiment_credit', 'input', 'number', lang('course_experiment_credit')),
+							array('course_weekly_period', 'input', 'number', lang('course_weekly_period')),
+							array('course_semester', 'input', 'text', lang('course_semester')),
+							array('course_language', 'input', 'text', lang('course_language')),
+							array('course_prelogic', 'textarea', '2', lang('course_prelogic')),
+							array('course_description', 'textarea', '5', lang('course_description')),
+							array('course_english_description', 'textarea', '5', lang('course_english_description')),
+						);
+					?>
+					<script>
+						GlobalVariables.course_info = <?= json_encode($course_info) ?>;
+					</script>
+					<?php
+						for ($i = 0; $i < count($course_info); $i++) {
+							$course = $course_info[$i];
+							echo('<div class="md-form md-outline">');
+							if ($course[1] == 'input') {
+								echo('<input type="' . $course[2] . '" id="' . $course[0] . '" class="form-control is-invalid" />');
+							} else if ($course[1] == 'textarea') {
+								echo('<textarea id="' . $course[0] . '" class="md-textarea form-control is-invalid" rows="' . $course[2] . '" style="resize:none;"></textarea>');
+							}
+							echo('<label for="' . $course[0] . '" data-error="wrong" data-success="right">' . $course[3] . '</label>');
+							echo('<div class="invalid-feedback"></div></div>');
+						}
+					?>
 				</div>
 				<div class="modal-footer">
 					<button id="add-course-submit" type="button" class="btn btn-primary btn-sm"><?= lang('submit') ?></button>
