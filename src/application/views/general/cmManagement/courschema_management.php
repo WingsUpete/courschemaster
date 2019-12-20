860f9f900bc92bb0a31ffba2dceca93b8c6a1803
Peter S <petershen815@126.com>
2019-12-21 02:57:55 +0800

    status check: courschema_management.php setup

--- src/application/views/general/cmManagement/courschema_management.php
+++ src/application/views/general/cmManagement/courschema_management.php
@@ -16,20 +16,94 @@
 <div id="courschema_management" class="main-content">
 	<h1 class="hide-for-now">Courschema Management</h1>
 	<hr class="hide-for-now" />
-	<!-- Button group -->
-	<div class="row pl-3 pr-3 mb-4">
-		<button type="button" class="btn btn-sm add" data-toggle="tooltip" data-title="<?= lang('add') ?>"><i class="fas fa-plus fa-lg"></i></button>
-		&ensp;&ensp;
-		<div>
-		<div class="input-group">
-			<div class="custom-file">
-				<input type="file" class="custom-file-input" id="choose-file" aria-describedby="upload-file" />
-				<label class="custom-file-label" for="choose-file"><?= lang('choose_file') ?></label>
-			</div>
-			<div class="input-group-append">
-				<span class="input-group-text" id="upload-file" data-toggle="tooltip" data-title="<?= lang('upload') ?>" data-placement="right"><i class="fas fa-upload fa-sm"></i></span>
+	<!-- card group -->
+	<div class="alert alert-info alert-dismissible fade show" role="alert">
+		<?= lang('courschema_tutorial') ?>
+		<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+	  		<span aria-hidden="true">&times;</span>
+		</button>
+	</div>
+	<div class="card">
+		<div class="card-header">
+			<ul class="nav nav-tabs card-header-tabs" id="list-tabs" role="tablist">
+				<li class="nav-item">
+					<a class="nav-link font-weight-bold active" id="specify_courschema" data-toggle="tab" title="<?= lang('specify_courschema') ?>" href="#specify_courschema-content" role="tab" aria-controls="specify_courschema-content" aria-selected="true"><?= lang('specify_courschema') ?></a>
+				</li>
+				<li class="nav-item disabled">
+					<a class="nav-link font-weight-bold" id="courschema_editor" data-toggle="tab" title="<?= lang('courschema_editor') ?>" href="#courschema_editor-content" role="tab" aria-controls="courschema_editor-content" aria-selected="false"><?= lang('courschema_editor') ?></a>
+				</li>
+			</ul>
+		</div>
+		<div class="card-body">
+			<div class="tab-content" id="list-tabs-content">
+				<div class="tab-pane fade show active" id="specify_courschema-content" role="tabpanel" aria-labelledby="specify_courschema">
+					<!-- add & upload -->
+					<div class="row pl-3 pr-3 mb-4">
+						<button type="button" class="btn btn-sm tool-btn new" data-toggle="tooltip" data-title="<?= lang('new') ?>"><i class="fas fa-plus fa-lg"></i></button>
+						&ensp;&ensp;
+						<button type="button" class="btn btn-sm tool-btn upload" data-toggle="tooltip" data-title="<?= lang('upload') ?>"><i class="fas fa-cloud-upload-alt fa-lg"></i></button>
+					</div>
+					<!-- select -->
+					<?php
+						$courschema_table_headers = array(
+							lang('courschema_id'), lang('courschema_name'), lang('courschema_major'), lang('courschema_department')
+						);
+					?>
+					<div class="table-responsive"><table id="courschemas-datatable" class="table table-bordered table-hover table-condensed text-center">
+						<thead>
+							<tr>
+								<?php
+									for ($i = 0; $i < count($courschema_table_headers); $i++) {
+										echo('<th class="th-sm justify-content-center">' . $courschema_table_headers[$i] . '</th>');
+									}
+								?>
+							</tr>
+						</thead>
+						<tbody></tbody>
+					</table></div>
+				</div>
+				<div class="tab-pane fade" id="courschema_editor-content" role="tabpanel" aria-labelledby="courschema_editor">
+					
+				</div>
 			</div>
 		</div>
+	</div>
+</div>
+
+<!-- courseWindow -->
+<div class="modal fade top" id="uploadWindow" tabindex="-1" role="dialog" aria-labelledby="uploadWindowLabel" aria-hidden="true">
+	<div class="modal-dialog modal-dialog-centered modal-lg side-modal modal-dialog-scrollable" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="uploadWindowLabel"><?= lang('upload') ?></h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<div class="modal-body">
+				<div>
+					<div class="input-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><?= lang('upload-courschema-support-file-types') ?></span>
+						</div>
+						<div class="custom-file">
+							<input type="file" multiple class="custom-file-input" id="choose-files" aria-describedby="submit-files" />
+							<label class="custom-file-label" for="choose-files"><?= lang('choose_file') ?></label>
+						</div>
+						<div class="input-group-append">
+							<span class="input-group-text" id="submit-files" title="<?= lang('submit') ?>"><i class="fas fa-upload fa-sm"></i></span>
+						</div>
+					</div>
+				</div>
+				<h3 class="h3-responsive font-weight-bold mt-3 pl-2"><code>cmh</code></h3>
+				<ul id="upload-cmh-list" class="list-group"></ul>
+				<h3 class="h3-responsive font-weight-bold mt-3 pl-2"><code>cmc</code></h3>
+				<ul id="upload-cmc-list" class="list-group"></ul>
+			</div>
+			<div class="modal-footer">
+				<button id="upload-courschema" type="button" class="btn btn-primary btn-sm"><?= lang('upload') ?></button>
+				<button type="button" class="btn btn-secondary btn-sm" data-dismiss="modal"><?= lang('close') ?></button>
+			</div>
 		</div>
 	</div>
 </div>
--- src/assets/js/cmManagement/courschema_management/courschema_management_helper.js
+++ src/assets/js/cmManagement/courschema_management/courschema_management_helper.js
@@ -10,9 +10,11 @@
      * @class CourschemaManagementHelper
      */
     function CourschemaManagementHelper() {
-        this.courses = [];
-		this.courseMap = {};
-		this.timeOutVars = {};
+        this.courschemas = [];
+		this.courschemaMap = {};
+		this.pendingBtn = '<span class="badge badge-warning badge-pill">' + SCLang.checking + '</span>';
+		this.cmcFiles = [];		
+		this.cmhFiles = [];		
     }
 
     /**
@@ -25,58 +27,53 @@
 		
 		//	table row
 		$(document).on('click', '#courses-datatable tbody tr', function() {
-			instance.resetModal();
-			instance.loadModal($(this));
-			$('#courseWindow').modal('show');
+			alert($(this).find('td').first().html());
 		});
 		
-		//	add
-		$(document).on('click', '.add', function() {
-			instance.resetModal();
-			$('#courseWindow').modal('show');
+		//	new
+		$(document).on('click', '.new', function() {
+			alert('new');
 		});
 		
-		//	input validators
-		$.each(GlobalVariables.course_info, function(index, course) {
-			instance.timeOutVars[course[0]] = null;
-			$('#' + course[0]).on('keyup', function() {
-				if (instance.timeOutVars[course[0]]) {
-					clearTimeout(instance.timeOutVars[course[0]]);
+		//	upload
+		$(document).on('click', '.upload', function() {
+			$('#uploadWindow').modal('show');
+		});
+		
+		//	upload files handling
+		$('#submit-files').click(function() {
+			var files = $('#choose-files').prop('files');	// FileList obj
+			var output_cmh = [];
+			var output_cmc = [];
+			instance.cmhFiles = [];
+			instance.cmcFiles = [];
+			$.each(files, function(index, file) {
+				var name = file.name;
+				var extension = name.substring(name.lastIndexOf('.')+1);
+				var html = '<li class="list-group-item d-flex justify-content-between align-items-center file-list-item"><span class="file-txt"><strong class="file-txt--name">' + name + '</strong> <small class="text-muted file-txt--addi">' + file.size + ' bytes, last modified: ' + (file.lastModifiedDate ? file.lastModifiedDate.toLocaleDateString() : 'n/a') + '</small></span>' + instance.pendingBtn + '</li>';
+				if (extension === 'cmh') {
+					output_cmh.push(html);
+					instance.cmhFiles.push(file);
+				} else if (extension === 'cmc') {
+					output_cmc.push(html);
+					instance.cmcFiles.push(file);
 				}
-				var obj = $(this);
-				instance.timeOutVars[course[0]] = setTimeout(function() {
-					if (!GeneralFunctions.checkEmpty(obj)) {
-						return;
-					}
-					//	true
-					obj.removeClass('is-valid, is-invalid').addClass('is-valid');
-				}, 300);
-			}).trigger('keyup');
-		});
-		
-		$('#add-course-submit').click(function() {
-			if ($('#courseWindow').find('input, textarea').hasClass('is-invalid')) {
-				GeneralFunctions.displayMessageAlert(SCLang.invalid_feedback, 'danger', 6000);
-				return;
-			}
-			instance.addCourse(
-				$('#course_code').val(), $('#course_chinese_name').val(), $('#course_english_name').val(),
-				$('#course_department').val(), $('#course_total_credit').val(), $('#course_experiment_credit').val(),
-				$('#course_weekly_period').val(), $('#course_semester').val(), $('#course_language').val(),
-				$('#course_prelogic').val(), $('#course_prelogic').val(), $('#course_english_desciption').val()
-			);
-		});
+			});
+			var emptyHTML = '<small class="text-muted ml-2">' + SCLang.no_record + '</small>';
+			$('#upload-cmh-list').html(output_cmh.length === 0 ? emptyHTML : output_cmh.join(''));
+			$('#upload-cmc-list').html(output_cmc.length === 0 ? emptyHTML : output_cmc.join(''));
+		}).trigger('click');
 		
 	};
 
 	//	Additional Methods
 	
 	/**
-     * get details of a question
+     * get details of courschemas
      */
-    CourschemaManagementHelper.prototype.retrieveCourses = function () {
+    CourschemaManagementHelper.prototype.retrieveCourschemas = function () {
 		//	AJAX
-        var postUrl = GlobalVariables.baseUrl + '/index.php/course_api/ajax_get_all_course_full_info';
+        var postUrl = GlobalVariables.baseUrl + '/index.php/all_courschemas_api/ajax_get_visible_courschema';
         var postData = {
             csrfToken: GlobalVariables.csrfToken
         };
@@ -88,99 +85,11 @@
             if (!GeneralFunctions.handleAjaxExceptions(response)) {
                 return;
             }
-			obj.courses = response;
+			obj.courschemas = response;
+			console.log(response);
 			
         }.bind(this), 'json').fail(GeneralFunctions.ajaxFailureHandler);
     };
-	
-	/**
-     * clear modal inputs
-     */
-    CourschemaManagementHelper.prototype.resetModal = function () {
-		$('#courseWindow').find('input, textarea').val('').trigger('keyup');
-    };
-	
-	/**
-     * load modal inputs
-     */
-    CourschemaManagementHelper.prototype.loadModal = function ($row) {
-		var course = this.courseMap[$row.find('td').first().html()];
-		$('#course_code').val(course.course_code);
-		$('#course_chinese_name').val(course.course_cn_name);
-		$('#course_english_name').val(course.course_en_name);
-		$('#course_department').val(course.department_code);
-		$('#course_total_credit').val(course.total_credit);
-		$('#course_experiment_credit').val(course.exp_credit);
-		$('#course_weekly_period').val(course.weekly_period);
-		$('#course_semester').val(course.semester);
-		$('#course_language').val(course.language);
-		$('#course_prelogic').val(course.pre_logic);
-		$('#course_description').val(course.cn_description);
-		$('#course_english_desciption').val(course.en_description);
-		$('#courseWindow').find('input, textarea').trigger('keyup');
-		$('#courseWindow').find('input + label, textarea + label').addClass('active');
-    };
-	
-	/**
-     * get details of a question
-     */
-    CourschemaManagementHelper.prototype.addCourse = function (code, chz_name, en_name, department_code, total_credit, exp_credit, weekly_period, semester, language, pre_logic, description, en_description) {
-		//	AJAX
-        var postUrl = GlobalVariables.baseUrl + '/index.php/course_api/ajax_add_one_course';
-        var postData = {
-            csrfToken: GlobalVariables.csrfToken,
-			code: JSON.stringify(code),
-			name: JSON.stringify(chz_name),
-			en_name: JSON.stringify(en_name),
-			department_code: JSON.stringify(department_code),
-			credit: total_credit,
-			exp_credit: exp_credit,
-			weekly_period: weekly_period,
-			semester: JSON.stringify(semester),
-			language: JSON.stringify(language),
-			pre_logic: JSON.stringify(pre_logic),
-			description: JSON.stringify(description),
-			en_description: JSON.stringify(en_description)
-        };
-		
-		var obj = this;
-		
-        return $.post(postUrl, postData, function (response) {
-			//	Test whether response is an exception or a warning
-            if (!GeneralFunctions.handleAjaxExceptions(response)) {
-                return;
-            }
-			
-			console.log(response.status === 'success');
-			
-			if (response.status === 'success') {
-				GeneralFunctions.displayMessageAlert(SCLang.add_course_success, 'success', 6000);
-				var course = {
-					course_code: code,
-					course_cn_name: chz_name,
-					course_en_name: en_name,
-					department_code: department_code,
-					total_credit: total_credit,
-					exp_credit: exp_credit,
-					weekly_period: weekly_period,
-					semester: semester,
-					language: language,
-					pre_logic: pre_logic,
-					cn_description: description,
-					en_description: en_description
-				};
-				obj.courses.push(course);
-				obj.courseMap[code] = course;
-			} else if (response.status === 'fail') {
-				GeneralFunctions.displayMessageAlert(SCLang.add_course_failure, 'danger', 6000);
-				console.error(response.message);
-			} else {
-				GeneralFunctions.displayMessageAlert('ABNORMAL RESPONSE IN QA-POST-QUESTIONS', 'warning', 60000);
-			}
-			
-        }.bind(this), 'json').fail(GeneralFunctions.ajaxFailureHandler);
-    };
-	
 	
     window.CourschemaManagementHelper = CourschemaManagementHelper;
 })();
