cca7158b3b7379905d8c414f409e0fc69b398f3e
Peter S <petershen815@126.com>
2019-12-22 05:40:15 +0800

    all_courschemas/all_courschemas_helper.js all retrieve and displayResults implemented, waiting for test data

--- src/assets/js/cmDisplay/all_courschemas/all_courschemas_helper.js
+++ src/assets/js/cmDisplay/all_courschemas/all_courschemas_helper.js
@@ -10,9 +10,6 @@
      * @class AllCourschemasHelper
      */
     function AllCourschemasHelper() {
-        this.departments = {};
-		this.majors = {};
-		this.courschemas = {};
 		this.stepper = undefined;
     }
 
@@ -25,7 +22,6 @@
 		// Listners
 		$(document).on('click', '.sel-dep-btns', function() {
 			var dep_id = $(this).prop('dataset').depId;
-			alert(dep_id);
 			instance.stepper.next();
 			instance.getMajors(dep_id);
 		});
@@ -75,32 +71,23 @@
                 return;
             }
 			
-			obj.displayDepartments('dep', response);
+			obj.displayResults('dep', response);
 			
         }.bind(this), 'json').fail(GeneralFunctions.ajaxFailureHandler);
     };
 
     /**
-     * Fill in Departments Data
-     */
-    AllCourschemasHelper.prototype.displayDepartments = function (level, items) {
-		$('#sel_' + level + ' .stepper-search-res .row').html('');
-		$.each(items, function(index, item) {
-			var html = '<div class="col-xs-12 col-lg-6 col-xl-4 search-res-item-block" data-filter="' + item.name + '"><div class="card search-res-item"><div class="card-header text-right">&ensp;</div><div class="card-body text-center"><h5 class="card-title font-weight-bold">' + ((level === 'ver') ? '<sup><a href="javascript:void(0);" class="collect text-warning" title="' + SCLang.collect + '"><i class="far fa-star fa-lg"></i></a></sup>&nbsp;' : '') + item.name + '</h5><hr /><button type="button" class="btn btn-outline-dark btn-block waves-effect font-weight-bold sel-' + level + '-btns sel-btn" data-dep-id="' + item.dep_id + '" data-dep-code="' + item.code + '" data-dep-name="' + item.name + '"><i class="fas fa-door-open"></i>&ensp;' + SCLang.access + '</button></div><div class="card-footer text-center text-muted">' + SCLang.number_of_majors + ': ' + item.number_of_majors + '</div></div></div>';
-			$('#sel_' + level + ' .stepper-search-res .row').append(html);
-		});
-    };
-
-    /**
      * Get All Majors
      */
-    AllCourschemasHelper.prototype.getMajors = function () {
+    AllCourschemasHelper.prototype.getMajors = function (dep_id) {
 		//	AJAX
         var postUrl = GlobalVariables.baseUrl + '/index.php/all_courschemas_api/ajax_get_maj';
         var postData = {
             csrfToken: GlobalVariables.csrfToken,
-			dep_id: 28
+			dep_id: dep_id
         };
+		
+		var obj = this;
 
         $.post(postUrl, postData, function (response) {
 			//	Test whether response is an exception or a warning
@@ -108,7 +95,7 @@
                 return;
             }
 			
-			console.log(response);
+			obj.displayResults('maj', response);
 			
         }.bind(this), 'json').fail(GeneralFunctions.ajaxFailureHandler);
     };
@@ -116,13 +103,15 @@
     /**
      * Get All Courschema Versions
      */
-    AllCourschemasHelper.prototype.getCourschemas = function () {
+    AllCourschemasHelper.prototype.getCourschemas = function (maj_id) {
 		//	AJAX
         var postUrl = GlobalVariables.baseUrl + '/index.php/all_courschemas_api/ajax_get_cm';
         var postData = {
             csrfToken: GlobalVariables.csrfToken,
-			maj_id: 1
+			maj_id: maj_id
         };
+		
+		var obj = this;
 
         $.post(postUrl, postData, function (response) {
 			//	Test whether response is an exception or a warning
@@ -130,9 +119,20 @@
                 return;
             }
 			
-			console.log(response);
+			obj.displayResults('ver', response);
 			
         }.bind(this), 'json').fail(GeneralFunctions.ajaxFailureHandler);
+    };
+
+    /**
+     * Fill in search Data
+     */
+    AllCourschemasHelper.prototype.displayResults = function (level, items) {
+		$('#sel_' + level + ' .stepper-search-res .row').html('');
+		$.each(items, function(index, item) {
+			var html = '<div class="col-xs-12 col-lg-6 col-xl-4 search-res-item-block" data-filter="' + item.name + '"><div class="card search-res-item"><div class="card-header text-right"> </div><div class="card-body text-center"><h5 class="card-title font-weight-bold">' + ((level === 'ver') ? '<sup><a href="javascript:void(0);" class="collect text-warning" title="' + SCLang.collect + '"><i class="far fa-star fa-lg"></i></a></sup> ' : '') + item.name + '</h5><hr /><button type="button" class="btn btn-outline-dark btn-block waves-effect font-weight-bold sel-' + level + '-btns sel-btn" data-dep-id="' + item.dep_id + '" data-dep-code="' + item.code + '" data-dep-name="' + item.name + '"><i class="fas fa-door-open"></i> ' + SCLang.access + '</button></div><div class="card-footer text-center text-muted">' + SCLang.number_of_majors + ': ' + item.number_of_majors + '</div></div></div>';
+			$('#sel_' + level + ' .stepper-search-res .row').append(html);
+		});
     };
 	
     window.AllCourschemasHelper = AllCourschemasHelper;
